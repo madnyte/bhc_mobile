d109958ddbc43ed68fc617f6410d831022c1701d
madnyte <motheo.ken@gmail.com>
2024-06-28 03:47:00 +0200

    update base url

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,8 +1,6 @@
-import 'dart:io';
-
 class Config {
-  static final baseUrl = String.fromEnvironment(
+  static const baseUrl = String.fromEnvironment(
     'baseUrl',
-    defaultValue: (Platform.isAndroid) ? '10.0.2.2:8090' : '127.0.0.1:8090',
+    defaultValue: 'https://bhc-backend-damp-forest-976.fly.dev',
   );
 }
